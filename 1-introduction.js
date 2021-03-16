0380478eb9e9174123ef5fa21b163422d54c274f
Navid Mahboobi <mahboobinavid9@gmail.com>
2021-03-16 13:52:59 +0100

    javascripting cmd

---
+++ 1-introduction.js
@@ -0,0 +1,18 @@
+//Exercise 1
+/*To keep things organized, let's create a folder for this workshop.
+  Run this command to make a directory called javascripting (or something
+  else if you like):
+     mkdir javascripting
+  Change directory into the javascripting folder:
+     cd javascripting
+  Create a file named introduction.js:
+     touch introduction.js
+  Or if you're on Windows:
+     type NUL > introduction.js
+  (type is part of the command!)
+  Open the file in your favorite editor, and add this text:
+     console.log('hello');
+*/
+
+
+console.log('hello');
---
+++ 9-if-statement.js
@@ -0,0 +1,13 @@
+//Exercise 9
+/*Create a file named number-to-string.js.
+  In that file define a variable named n that references the number 128;
+  Call the .toString() method on the n variable.
+  Use console.log() to print the results of the .toString() method to the
+  terminal.*/
+
+var fruit = 'orange';
+if (fruit.length > 5) {
+    console.log('The fruit name has more than five characters.');
+} else {
+    console.log('The fruit name has five characters or less.');
+}
